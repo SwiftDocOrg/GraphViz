eb3d046a0616fa0e5803ed874f8884b130258721
Mattt <mattt@me.com>
2020-03-20 05:14:28 -0700

    Add string quoted helper property

--- Sources/DOT/Extensions/String+Extensions.swift
+++ Sources/DOT/Extensions/String+Extensions.swift
@@ -3,6 +3,10 @@ extension String {
         return self.replacingOccurrences(of: "\"", with: #"\""#)
     }
 
+    var quoted: String {
+        return #""\#(self)""#
+    }
+
     func indented(by spaces: Int = 2) -> String {
         return split(separator: "\n", omittingEmptySubsequences: false)
                 .map { String(repeating: " ", count: spaces) + $0 }
