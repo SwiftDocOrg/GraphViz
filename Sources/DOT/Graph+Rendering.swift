52028f4b93910471f7d396971ed88cee6788c9d7
Mattt <mattt@me.com>
2020-03-27 06:04:50 -0700

    Use which to determine path of graphviz executables

--- Sources/DOT/Graph+Rendering.swift
+++ Sources/DOT/Graph+Rendering.swift
@@ -3,12 +3,37 @@ import GraphViz
 
 // MARK: - Render
 
+fileprivate func which(_ command: String) throws -> URL {
+    let task = Process()
+    let url = URL(fileURLWithPath: "/usr/bin/which")
+    if #available(OSX 10.13, *) {
+        task.executableURL = url
+    } else {
+        task.launchPath = url.path
+    }
+
+    task.arguments = [command.trimmingCharacters(in: .whitespacesAndNewlines)]
+
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    if #available(OSX 10.13, *) {
+        try task.run()
+    } else {
+        task.launch()
+    }
+
+    task.waitUntilExit()
+
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let string = String(data: data, encoding: .utf8)!.trimmingCharacters(in: .whitespacesAndNewlines)
+    return URL(fileURLWithPath: string)
+}
+
 extension Graph {
     public func render(using layout: LayoutAlgorithm, to format: Format) throws -> Data {
         let encoded = DOTEncoder().encode(self)
 
-        let url = URL(fileURLWithPath: "/usr/local/bin/").appendingPathComponent(layout.rawValue)
-
+        let url = try which(layout.rawValue)
         let task = Process()
         if #available(OSX 10.13, *) {
             task.executableURL = url
@@ -26,7 +51,7 @@ extension Graph {
         task.standardInput = inputPipe
 
         var data = Data()
-        
+
         let outputPipe = Pipe()
         defer { outputPipe.fileHandleForReading.closeFile() }
         outputPipe.fileHandleForReading.readabilityHandler = { fileHandle in
